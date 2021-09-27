244dc5e72e9c3fb03763f29382ac3ac19579ae86
NaturaAdnyana <natura538@gmail.com>
2021-09-27 20:24:36 +0800

    trying to make function factories

--- script.js
+++ script.js
@@ -1,56 +1,15 @@
-// Constructor Function
-// function Mahasiswa(nama, energi) {
-//   this.nama = nama;
-//   this.energi = energi;
-// }
-
-// Mahasiswa.prototype.makan = function (porsi) {
-//   this.energi += porsi;
-//   return `Halo ${this.nama}, selamat makan!`;
-// }
-
-// Mahasiswa.prototype.main = function (jam) {
-//   this.energi -= jam;
-//   return `Halo ${this.nama}, selamat bermain!`;
-// }
-
-// Mahasiswa.prototype.tidur = function (jam) {
-//   this.energi += jam * 2;
-//   return `Halo ${this.nama}, selamat tidur!`;
-// }
-
-// keyword new
-// let Natura = new Mahasiswa('Natura', 10);
-
-// tes on console
-// Natura
-// Natura.main(5)
-// Natura
-
-// ===================================================
-
-// Class was made from construction function with lot of prototype
-
-class Mahasiswa {
-  // property
-  constructor(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-  }
-
-  // method
-  makan (porsi) {
-    this.energi += porsi;
-    return `Halo ${this.nama}, selamat makan!`;
-  }
-  main (jam) {
-    this.energi -= jam;
-    return `Halo ${this.nama}, selamat bermain!`;
-  }
-  tidur (jam) {
-    this.energi += jam * 2;
-    return `Halo ${this.nama}, selamat tidur!`;
+// this function return function
+function ucapanSalam(waktu) {
+  return function (nama) {
+    console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
   }
 }
 
-let Natura = new Mahasiswa('Natura', 10);
+// function factories
+let selamatPagi = ucapanSalam('pagi');
+let selamatSiang = ucapanSalam('siang');
+let selamatMalam = ucapanSalam('malam');
+
+selamatPagi('Natura');
+
+// Output : Halo Natura, selamat pagi, semoga harimu menyenangkan!
