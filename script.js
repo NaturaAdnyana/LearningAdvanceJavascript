243be55f93fd296d599df4abf1687945ad199471
NaturaAdnyana <natura538@gmail.com>
2021-10-03 18:52:17 +0800

    making method chaining from filter, map, and reduce

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Higher Order Function adalah function yang mengambil function didalamnya
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-// jumlahkan seluruh elemen pada array
-// reduce perlu 2 parameter
-const newAngka = angka.reduce((accumulator, currentValue) => 
-accumulator + currentValue, 0);
-// nilai akhir defaultnya 0
-console.log(newAngka);
+// Cari angka > 5
+// Kalikan 3
+// Jumlahkan
+const hasil = angka.filter(a => a > 5) // 8, 9, 9
+  .map(a => a * 3) // 24, 27, 27
+  .reduce((acc, cur) => acc + cur); // 78
+
+console.log(hasil);
