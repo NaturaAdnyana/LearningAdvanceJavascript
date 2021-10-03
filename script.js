7f0bad10c66800a3489cb7d78f0977c4013c7e1a
NaturaAdnyana <natura538@gmail.com>
2021-10-03 18:25:29 +0800

    trying reduce higher order function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Higher Order Function adalah function yang mengambil function didalamnya
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-// kalikan semua angka dengan 2
-const newAngka = angka.map(a => a * 2);
+// jumlahkan seluruh elemen pada array
+// reduce perlu 2 parameter
+const newAngka = angka.reduce((accumulator, currentValue) => 
+accumulator + currentValue, 0);
+// nilai akhir defaultnya 0
 console.log(newAngka);
