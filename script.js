00f4081be9e9468219d1c658f7202321105cc235
NaturaAdnyana <natura538@gmail.com>
2021-10-03 12:03:20 +0800

    making arrow function

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-// let add = function () {
-//   let counter = 0;
-//   // variabel counter disimpan didalam function, sehingga tidak terpengaruh oleh global
-//   return function () {
-//     return ++counter;
-//   };
-// }
+// === Jika sama sekali tidak punya parameter ===
+const tampilUcapan = () => `Hello World`;
+console.log(tampilUcapan());
 
-// counter = 10;
-// scope global mencoba mempengaruhi variabel counter
 
-// let a = add();
+// === Jika parameter cuma 1 ===
+// const tampilNama = (nama) => { return `Halo, $(nama)`; }
+// console.log('Natura Adnyana');
 
-// console.log(a());
-// console.log(a());
-// console.log(a());
+// v v v Bahkan bisa lebih ringkas v v v
 
-// v v v v v v
+const tampilNama = nama => `Halo, $(nama)`;
+console.log('Natura Adnyana');
 
-// Immediately Invoked Function (IIFE)
-let add = (function () {
-  // function dibungkus kurung, sehingga menjadi Immediately Invoked Function
-  let counter = 0;
-  // variabel counter disimpan didalam function, sehingga tidak terpengaruh oleh global
-  return function () {
-    return ++counter;
-  };
-})();
-// diakhir diisi kurung kosong
 
-counter = 10;
-// scope global mencoba mempengaruhi variabel counter
+// === Jika parameter lebih dari 1 ===
+const tampilSalam = (nama, waktu) => {
+  return `Selamat ${waktu}, ${nama}`;
+}
+console.log(tampilSalam('Natura', 'Siang'));
 
-console.log(add());
-console.log(add());
+// Contoh arrow function yang lebih rumit
+
+let mahasiswa = ['Natura', 'Krisna', 'Deta', 'Astawa'];
+
+// v v v sebelum jadi arrow function (function expression) v v v
+// let jumlahHuruf = mahasiswa.map(function (nama) {
+//   return nama.length;
+// });
+// console.log(jumlahHuruf);
+
+// v v v sesudah menjadi arrow function v v v
+// let jumlahHuruf = mahasiswa.map(nama => nama.length);
+// console.log(jumlahHuruf);
+
+// v v v jika ingin mengembalikan objek v v v
+// let jumlahHuruf = mahasiswa.map(nama => ({ nama: nama, jumlahHuruf: nama.length }));
+let jumlahHuruf = mahasiswa.map(nama => ({ nama, jumlahHuruf: nama.length }));
+console.table(jumlahHuruf);
