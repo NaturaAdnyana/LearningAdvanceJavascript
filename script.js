1056ad7e1448bb6d00e62f6dea615f1ade9133a7
NaturaAdnyana <natura538@gmail.com>
2021-10-03 18:54:07 +0800

    explaining method chaining

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Higher Order Function adalah function yang mengambil function didalamnya
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
+// method chaining adalah gabungan / rantai function-function yang tersambung
+
 // Cari angka > 5
 // Kalikan 3
 // Jumlahkan
