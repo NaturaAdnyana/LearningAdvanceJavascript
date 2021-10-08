a703cb07ff9c40d1e9c04ea2dbacd33ce899400d
NaturaAdnyana <natura538@gmail.com>
2021-10-08 12:11:24 +0800

    implementing higher order func into real life case

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 // Higher Order Function adalah function yang mengambil function didalamnya
-const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-// method chaining adalah gabungan / rantai function-function yang tersambung
+// ambil semua elemen video
+const videos = Array.from(document.querySelectorAll('[data-duration]'));
 
-// Cari angka > 5
-// Kalikan 3
-// Jumlahkan
-const hasil = angka.filter(a => a > 5) // 8, 9, 9
-  .map(a => a * 3) // 24, 27, 27
-  .reduce((acc, cur) => acc + cur); // 78
+// yang dipilih hanya Javascript Lanjutan
+let jsLanjut = videos.filter(video => video.textContent.includes("JAVASCRIPT LANJUTAN"))
 
-console.log(hasil);
+// menerapkan method chaining
+// ambil durasi masing-masing video
+  .map(item => item.dataset.duration)
+
+// ubah durasi dari string menjadi float, ubah menit menjadi detik
+  .map(waktu => {
+    // 10:30 -> [10, 30] split
+    // parseFloat -> ubah string menjadi float
+    const parts = waktu.split(':').map(part=> parseFloat(part));
+    return (parts[0] * 60) + parts[1];
+  })
+
+// jumlahkan semua detiknya
+  .reduce((total, detik) => total + detik);
+
+// ubah formatnya jadi jam menit detik
+const jam = Math.floor(jsLanjut / 3600);
+jsLanjut = jsLanjut - jam * 3600;
+const menit = Math.floor(jsLanjut / 60);
+const detik = jsLanjut - menit * 60;
+
+// simpan di DOM
+const pDurasi = document.querySelector('.total-durasi');
+pDurasi.textContent = `${jam} jam, ${menit} menit, ${detik} detik`;
+
+const jumlahVideo = videos.filter(video => video.textContent.includes("JAVASCRIPT LANJUTAN")).length;
+
+const pJumlahVideo = document.querySelector('.jumlah-video');
+pJumlahVideo.textContent = `${jumlahVideo} video`;
