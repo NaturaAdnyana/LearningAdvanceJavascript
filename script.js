655d2fa2b0da45d7f8d796dedf03d6a914703520
NaturaAdnyana <natura538@gmail.com>
2021-11-07 23:36:49 +0800

    Introducing promise & fetch

--- script.js
+++ script.js
@@ -1,43 +1,86 @@
-// Callback
-// Synchronous Callback
+// There is the easiest way to get data from api
 
-// function halo(nama) {
-//   alert(`Halo, ${nama}`);
-// }
+// JANJI (terpenuhi / ingkar)
+// states (fulfilled / rejected / pending)
+// callback (resolve / reject / finally)
+// ada aksi (then / catch)
 
-// function tampilkanPesan(callback) {
-//   const nama = prompt('Masukan nama : ');
-//   callback(nama);
-// }
+// use fetch!
+// fetch('http://www.omdbapi.com/?apikey=21cb464f&s=avengers')
+  // .then(response => response.json())
+  // .then(response => console.log(response));
 
-// tampilkanPesan(halo);
-
-// Asynchronous Callback
-function getDataMahasiswa(url, success, error) {
-  let xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        success(xhr.response);
-      }
-      else if (xhr.status === 404) {
-        error();
-      }
-    }
+// introduce promise
+let ditepati = true;
+const janji1 = new Promise((resolve, reject) => {
+  if (ditepati) {
+    resolve('Janji telah ditepati!')
   }
-  xhr.open('get', url);
-  xhr.send();
-}
+  else {
+    reject('Ingkar janji');
+  }
+});
 
-console.log('start');
+janji1
+  .then(response => console.log('OK : ' + response))
+  .catch(response => console.log('NOT OK : ' + response));
 
-getDataMahasiswa('data/data.json', results => {
-  // console.log(JSON.parse(results));
-  const mhs = JSON.parse(results);
-  mhs.forEach(m => console.log(m.nama));
-}, () => {
+// another example with pending
+let ditepati2 = true;
+const janji2 = new Promise((resolve, reject) => {
+  if (ditepati2) {
+    setTimeout(() => {
+      resolve('Ditepati setelah beberapa waktu')
+    }, 2000);
+  }
+  else {
+    setTimeout(() => {
+      resolve('Tidak ditepati setelah beberapa waktu')
+    }, 2000);
+  }
+});
 
-})
+console.log('mulai');
+// with pending status appear in console
+// console.log(janji2.then(() => console.log(janji2)));
+// with no pending status appear in console
+janji2
+  .finally(() => console.log('selesai menunggu'))
+  .then(response => console.log('OK : ' + response))
+  .catch(response => console.log('NOT OK : ' + response));
+console.log('selesai');
 
-console.log('end');
+// you know promise.all ?
+const film = new Promise(resolve => {
+  setTimeout(() => {
+    resolve([{
+      judul: 'Avenger',
+      sutradara: 'Natura'
+    }])
+  }, 3000);
+});
+
+const cuaca = new Promise(resolve => {
+  setTimeout(() => {
+    resolve([{
+      kota: 'Bandung',
+      temp: 'Gianyar'
+    }]);
+  }, 2500);
+});
+
+// Don't do this
+// film.then(response => console.log(response));
+// cuaca.then(response => console.log(response));
+
+// Do this
+Promise.all([film, cuaca])
+  // this will combine 2 arrays
+  // .then(response => console.log(response));
+
+  // this will seperate 2 arrays
+  .then(response => {
+    const [film, cuaca] = response;
+    console.log(film);
+    console.log(cuaca);
+  })
