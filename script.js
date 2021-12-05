106f54ec9bda8cf73b8e7446defc5f3fd3a0b4e8
NaturaAdnyana <natura538@gmail.com>
2021-12-05 22:15:26 +0800

    introduce use strict

--- script.js
+++ script.js
@@ -1,48 +1,12 @@
-// before using async await
+// add this to use strict mode
+"use strict";
 
-// const coba = new Promise(resolve => {
-//   setTimeout(() => {
-//     resolve('selesai')
-//   }, 2000);
-// });
+x = 9; //this line will be error, it needs to declared first
+tampilkanAngka(); //this line will be error, it needs to declared first
 
-// coba.then(() => console.log(coba));
-
-// after using async await
-
-function cobaPromise() {
-  return new Promise((resolve, reject) => {
-    const waktu = 3000;
-    if (waktu < 5000) {
-      setTimeout(() => {
-        resolve('selesai')
-      }, 2000);
-    }
-    else {
-      reject('kelamaan');
-    }
-  });
+function tampilkanAngka() {
+  var x = 9;
+  console.log(x);
 }
 
-// before using async await function
-const coba = cobaPromise();
-coba.then(coba => console.log(coba));
-coba.catch(coba => console.log(coba));
-
-// after async await
-// async function cobaAsync() {
-//   const coba = await cobaPromise();
-//   console.log(coba);
-// }
-
-async function cobaAsync() {
-  try {
-    const coba = await cobaPromise();
-    console.log(coba);
-  }
-  catch (err) {
-    console.log(err);
-  }
-}
-
-cobaAsync();
+// so, there is no hoisting again
